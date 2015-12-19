406c4e2044a4878fb2648351e673e6fb9fd891ad
unknown <atwede@axis41.com>
2015-12-19 14:48:20 -0700

    testing webtask hook3

--- webtask.js
+++ webtask.js
@@ -1,6 +1,5 @@
 module.exports = function(context, callback) {
   // context.webhook contains the webhook payload provided by GitHub
   // context.data contains URL query and webtask token parameters
-  console.log("testing webtask from github hook");
-  callback(null, {result : "success"});
+  callback(null, { some: 'result' });
 }
