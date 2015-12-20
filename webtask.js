fbed3f6bcbcdcc1c5ef450558dcc245cd03b2c02
unknown <atwede@axis41.com>
2015-12-19 20:55:14 -0700

    testing webtask hook

--- webtask.js
+++ webtask.js
@@ -1,8 +1,8 @@
 
 return function (context, callback) {
 	// console.dir("test");
-	// console.debug("test1");
+	console.debug("test1");
 	console.log("test2");
     // return callback(null, {result : context.webhook.repository.full_name});
-    return callback(null, {result : "test 123456"});
+    return callback(null, {result : "test 123457"});
 }
