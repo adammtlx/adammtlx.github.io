3cee33f6b4045a8b47ccc4843128d17359be3502
unknown <atwede@axis41.com>
2015-12-19 20:05:49 -0700

    testing webtask hook

--- webtask.js
+++ webtask.js
@@ -2,9 +2,10 @@ var fs = require("fs");
 
 return function (context, callback) {
     console.log('testing webtask');
-    // var fileNames;
+    var fileNames;
     fs.readdir(".", function(err, files) {
-    	// fileNames = files;
+    	fileNames = files;
+    	// return callback(null, {result : files});
     });
     return callback(null, {result : "testing"});
 }
